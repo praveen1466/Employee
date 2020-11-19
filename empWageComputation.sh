0d08b9664a5fc84b5723b59d011d5ea0f77182dd
praveen1466 <praveensknl7@gmail.com>
2020-11-18 21:56:52 -0800

    Day and daily wage along with total wage is updated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -188,3 +188,13 @@ done
 totalSalary=$(( $totalWorkHours * $EMP_RATE_PER_HR ));
 echo -e "\n::::::::::::Printing Days from array::::::"
 echo ${!empDailyWage[@]}
+
+echo -e "\n::::::::::::Printing salary stored day wise in an array:::::::::::::::"
+echo ${empDailyWage[@]}
+
+echo -e "\n::::::::::::Printing Days from dictionary::::::"
+echo ${!dayWiseWages[@]}
+
+echo -e "\n::::::::::::Printing salary stored day wise in dictionary:::::::::::::::"
+echo ${dayWiseWages[@]}
+echo "Total salary: $totalSalary"
