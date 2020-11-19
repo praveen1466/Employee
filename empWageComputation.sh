c36c48c51f07b0dbb5c7e9a62bda30d1c6854a1a
praveen1466 <praveensknl7@gmail.com>
2020-11-18 21:11:19 -0800

    Added: employee is present/absent Logic added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
 Welcome Message
+
+#! /bin/bash
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
+then
+echo "Employee is Present!"
+else
+echo "Employee is absent!"
+fi
