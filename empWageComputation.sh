3aa87338a32871763ab528ec6a4fd89278c47e7b
praveen1466 <praveensknl7@gmail.com>
2020-11-18 21:50:13 -0800

    Added: daily wage along with total wage is updated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -163,3 +163,28 @@ $IS_PART_TIME ) empHrs=4
 esac
 echo $empHrs
 }
+
+
+
+
+while(( $totalWorkHours < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
+do
+((totalWorkingDays++))
+#Calling function getWorkingHours by passing random number
+workHours="$( getWorkingHours $(( RANDOM%3 )) )"
+
+echo "Work hours from function getWorkingHours()::::::::::>>> $workHours "
+totalWorkHours=$(( $totalWorkHours + $workHours ))
+echo -e "TotalWorkHours: $totalWorkHours\n"
+
+#Using array to store day-wise salary
+empDailyWage[$totalWorkingDays]="$( calculateDailyWage $workHours )"
+
+#Using dictionary to store day-wise salary
+dayWiseWages[$totalWorkingDays]="$(calculateDailyWage $workHours)"
+
+done
+
+totalSalary=$(( $totalWorkHours * $EMP_RATE_PER_HR ));
+echo -e "\n::::::::::::Printing Days from array::::::"
+echo ${!empDailyWage[@]}
